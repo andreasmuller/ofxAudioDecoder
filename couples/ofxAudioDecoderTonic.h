653a25b21e6ea573c3b554fad4b808406ca97464
Elie Zananiri <prisonerjohn@gmail.com>
2015-04-23 17:24:01 -0400

    Made functions static so they can be used multiple times.

--- couples/ofxAudioDecoderTonic.h
+++ couples/ofxAudioDecoderTonic.h
@@ -1,6 +1,6 @@
 //
 //  ofxAudioDecoderTonic.h
-//  Colony
+//  ofxAudioDecoder
 //
 //  Created by Elias Zananiri on 2015-04-23.
 //
@@ -12,14 +12,14 @@
 #include "ofxAudioDecoder.h"
 #include "SampleTable.h" // Tonic
 
-Tonic::SampleTable ofxAudioDecoderTonic(ofxAudioDecoder& audioDecoder)
+static Tonic::SampleTable ofxAudioDecoderTonic(ofxAudioDecoder& audioDecoder)
 {
     Tonic::SampleTable sampleTable = Tonic::SampleTable(audioDecoder.getNumFrames(), audioDecoder.getChannels());
     memcpy(sampleTable.dataPointer(), audioDecoder.getRawSamples().data(), audioDecoder.getNumSamples() * sizeof(float));
     return sampleTable;
 }
 
-Tonic::SampleTable ofxAudioDecoderTonic(const string& path)
+static Tonic::SampleTable ofxAudioDecoderTonic(const string& path)
 {
     ofxAudioDecoder audioDecoder;
     audioDecoder.load(path);
